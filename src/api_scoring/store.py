9bac9db77c34386371e8c7d31a84662b9206d8f5
aleksei <bscadet@yandex.ru>
2025-05-11 00:57:17 +0300

    add redis to Github Actions 	modified:   .github/workflows/tests.yml 	modified:   src/api_scoring/store.py

--- src/api_scoring/store.py
+++ src/api_scoring/store.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 
 class Store:
     def __init__(self, host='localhost', port=6379, db=0, retries=3, timeout=1):
-        self.host = host
-        self.port = port
+        self.host = 'localhost'
+        self.port = 6379
         self.db = db
         self.retries = retries
         self.timeout = timeout
